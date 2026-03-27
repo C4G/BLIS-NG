e0c627407302e483b7b7b37aeac5520ce40bc777
Haki Atalov <haki.atalov@gmail.com>
2026-03-26 20:54:37 -0400

    Add environment variables for storage and data directories in Apache config

--- Config/HttpdConf.cs
+++ Config/HttpdConf.cs
@@ -11,6 +11,8 @@ public class HttpdConf : ConfigurationFile
     public readonly string DOCROOT;
     public readonly string PID_FILE;
     public readonly string LOCAL_DIR;
+    public readonly string STORAGE_DIR;
+    public readonly string DATA_DIR;
 
     public HttpdConf() : base(new Uri("avares://BLIS-NG/Assets/Templates/httpd.conf.liquid"))
     {
@@ -19,6 +21,8 @@ public class HttpdConf : ConfigurationFile
         DOCROOT = Path.Combine(BASE_DIR, "releases", "dev", "htdocs");
         PID_FILE = Path.Combine(TMP_DIR, "httpd.pid");
         LOCAL_DIR = Path.Combine(BASE_DIR, "local");
+        STORAGE_DIR = Path.Combine(BASE_DIR, "storage");
+        DATA_DIR = Path.Combine(BASE_DIR, "data");
     }
 
     public override void Write()
@@ -31,8 +35,9 @@ public class HttpdConf : ConfigurationFile
             { "log_dir", LOG_DIR },
             { "server_dir", SERVER_BASE_DIR },
             { "pid_file", PID_FILE },
-            { "local_dir", LOCAL_DIR }
-
+            { "local_dir", LOCAL_DIR },
+            { "storage_dir", STORAGE_DIR },
+            { "data_dir", DATA_DIR }
         });
     }
 }
